628bc7ab96e847f97e75c86228844f72a321d175
NigelT97 <nigelmj97@gmail.com>
2024-03-10 10:39:24 +0200

    add args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -19,16 +19,24 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
 
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
+        """Return the dictionary of the  instance.
 
-        Includes the key/value pair __class__ representing
-        the class name of the object.
         """
         r_dict = self.__dict__.copy()
         r_dict["created_at"] = self.created_at.isoformat()
@@ -37,6 +45,6 @@ class BaseModel:
         return r_dict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """Return the print/str representation instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
